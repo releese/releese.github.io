dd26ab1837b6590bc88f644e361033c50192ffdf
releese <rilees@ttu.ee>
2019-10-13 12:45:10 +0300

    too proto

---
+++ scripts/landing.js
@@ -0,0 +1,12 @@
+function sendWish() {
+	if (document.getElementById('wish_box').value === "") {
+		document.getElementById('thank_you_message').innerText = "Please insert a message first!"
+	} else {
+		document.getElementById('thank_you_message').innerText = "Thank you for your wish! You made someone happy! :)";
+	}
+	document.getElementById('wish_box').value = "";
+	document.getElementById('thank_you_message').classList.add('visible');
+	setTimeout(function(){
+			document.getElementById('thank_you_message').classList.remove('visible');
+}, 2500);
+}
